78fc07af31a322b1843627d55ce4c8262a76e528
Noah Hewitt <invalidunits@gmail.com>
2024-01-24 21:55:32 -0500

    Completely refactor coinslot

--- arcade/arcade.ino
+++ arcade/arcade.ino
@@ -3,52 +3,20 @@
 // 2023 December 20-XX
 
 
-// #define COINSLOT_PIN PIN2
-// #define COINSLOT_NC_SWITCH
+#include "coinslot.hpp"
 
-#include "coinslot.h"
 
-// Timing variables
-unsigned long           last_time = 0; // The time of the last update.
-unsigned long           cs_timer = 0; // elapsed time since the last signal
-
-// This is incremented during a coinslot interupt.
-// If we timeout, the amount of signals are processed and sent.
-CS_VOLATILE uint8_t cs_sig_count = 0; 
-
-// constant variables
-constexpr unsigned long cs_timeout = 100; // The amount of time it takes to 
-constexpr auto arcade_baud_rate = 115200; // The baud rate of arcade application 
+using CSLOT = Coin::CoinSlot<2>;
+constexpr auto baud_rate =      115200; // The baud rate of arcade application 
+constexpr bool normally_open =  false;
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
-    Serial.begin(arcade_baud_rate);
-    while (!Serial); // Wait for a Serial named Godot
-
-    CS_attachCoinSignal(coinSignal);
-    interrupts();
+    Serial.begin(baud_rate);
+    CSLOT::setup(normally_open);
 }
 
-
-void coinSignal() {
-    ++cs_sig_count;
-    if (((cs_sig_count + 1)<cs_coin_values_size)) 
-        cs_sig_count = cs_coin_values_size - 1;
-
-    cs_timer = 0;
-}
-
-void loop() { 
-    // Determine the time
-    auto time = millis();
-    cs_timer += time - last_time;
-    last_time = time;
-
-
-    if (cs_timer >= cs_timeout) {
-        const auto value = String(CS_VALUE_FROM_SIGNAL(cs_sig_count));
-        Serial.print("[C" + value + "]");
-        cs_sig_count = 0;
-        cs_timer = 0;
+void loop() {
+    if (auto coin = CSLOT::retrieve_coin()) {
+        Serial.print("[C" + String(coin) + "]"); 
     }
 }
---
+++ arcade/coinslot.hpp
@@ -0,0 +1,108 @@
+
+/*
+ * Created Date: Saturday, December 23rd 2023, 11:23:33 pm
+ * Author: Noah Hewitt
+ * 
+ * Copyright (c) 2024 Noah Hewitt
+ */
+
+#ifndef _ARDUINO_COINSLOT
+#define _ARDUINO_COINSLOT
+
+
+
+/* 
+    This is a helper Header only Library to work HX-196 Universal Coin Slot
+*/
+namespace Coin {
+    #ifndef COIN_VALUE(signal)
+        /// @brief The value of each coin per signal
+        constexpr PROGMEM uint8_t default_coin_values[] = {
+            0,
+            5,
+            10,
+            25,
+            100,
+            200,
+        };
+    
+    // Because 
+    #define COIN_VALUE(signal) pgm_read_byte(default_coin_values + signal); 
+    #endif 
+
+    
+    constexpr uint8_t max_coin_signal = sizeof(COIN_VALUE)/sizeof(*COIN_VALUE);
+
+
+    /// @brief 
+    /// This is one of the many reasons why I (sometimes) hate C++
+    /// Maybe I shouldn't of made it this way... whatever...
+    ///
+    /// This is a static class that takes in a pin as a template parameter.
+    /// Each instance of this classes use with a pin is technically... a different class.
+    /// I abuse this functionality to generate a whole sort of variables and modifiers.
+    ///
+    ///
+    /// @tparam coinslot_pin 
+    /// The digital pin shorted to the COIN pin of the coinslot you wish to use.
+    /// This pin must be a functional interrupt pin.
+    template <uint8_t coinslot_pin = 2> class CoinSlot {
+        public:
+            static constexpr uint8_t interupt = digitalPinToInterrupt(coinslot_pin);
+            static constexpr unsigned long timeout = 500; //ms
+
+            static_assert(interupt != NOT_AN_INTERRUPT, "Pin must be an interupt pin to work with Coin Slot");
+
+            /// @brief Setup the coinlots functionality 
+            /// @param normally_open 
+            // Set if the coinslot is set to normally open or normally closed.
+            // This value should match the switch on the physical
+            // I would recommnnd normally closed to prevent false signals during the
+            // beginning of execution
+            static void setup(bool normally_open = false) {
+                if (has_setup) return;
+                
+                pinMode(coinslot_pin, INPUT_PULLUP);
+                attachInterrupt(interupt, signal, normally_open? FALLING : RISING);
+                has_setup = true;
+            }
+
+            /// @brief  Update the coin slot and return the value of coins
+            /// @return Returns the value of coins inserted into the coinslot, Otherwise zero. 
+            static uint8_t retrieve_coin() {
+                uint8_t value = 0;
+                if (!has_setup)
+                    return value;
+
+                
+                if ((millis() - timer) > timeout) {
+                    noInterrupts(); // Disable interupts to
+                        sig_count %= max_coin_signal;
+                        value = COIN_VALUE(sig_count);
+                        sig_count = 0;
+                  		timer = millis();
+                    interrupts();
+                    
+                }
+
+                return value;
+            }
+
+        private:
+            CoinSlot();
+            static void signal() {
+                if (!has_setup)
+                    return value;
+
+                sig_count += 1;
+                timer = millis();
+            }
+
+            inline static bool               has_setup = false;
+            inline static volatile uint8_t        sig_count = 0;
+            inline static volatile unsigned long timer = 0;
+
+    };
+}
+
+#endif // _ARDUINO_COINSLOT
